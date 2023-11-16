04f53fc812a5bfb316cfed4cbe02b56589ab9206
qngg <124476435+qngg@users.noreply.github.com>
2023-11-16 11:41:28 +0000

    cleaned up js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import md from 'markdown-it'
-var singleline = md.renderInline('__markdown-it__ rulezz!'); //single line
-
-document.getElementById("test1").innerHTML = singleline;
-
-var multiline = md.render('# markdown-it rulezz!');
-
-document.getElementById().innerHTML = multiline;
+// nothing to do here
