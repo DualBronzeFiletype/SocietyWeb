503f2410375496e30ed9a746645dc09529aa210c
qngg <2021xinqing@gmail.com>
2023-11-27 20:51:20 +0800

    launch configurated and better qol for local tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// nothing to do here
+// For those who are running in locally
+if (window.location.protocol === "file:")
+{
+    let anchorTags = document.getElementsByTagName("a"); // Get all anchor tags
+    anchorTags.array.forEach(anchor => 
+    {
+        if (anchor.href[-1] == "/" || anchor.href[-1] == "\\")
+        {
+            anchor.href += "index.html";
+        }
+        else
+        {
+            anchor.href += "/index.html";
+        }
+    });
+}
+
+// If you're not running locally, that above won't run
