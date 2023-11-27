ae9ffaab6965ec36611426fb6e2c1b634fcd8120
qngg <2021xinqing@gmail.com>
2023-11-27 20:56:07 +0800

    link jquery to all pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 if (window.location.protocol === "file:")
 {
     let anchorTags = document.getElementsByTagName("a"); // Get all anchor tags
-    anchorTags.array.forEach(anchor => 
+    for (let anchor of anchorTags)
     {
         if (anchor.href[-1] == "/" || anchor.href[-1] == "\\")
         {
@@ -12,7 +12,7 @@ if (window.location.protocol === "file:")
         {
             anchor.href += "/index.html";
         }
-    });
+    }
 }
 
 // If you're not running locally, that above won't run
