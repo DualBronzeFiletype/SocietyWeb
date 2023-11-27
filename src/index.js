56a39863d3118708eb33b2e5ffc680849e8798c2
qngg <2021xinqing@gmail.com>
2023-11-27 21:14:30 +0800

    hotfix?

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ if (window.location.protocol === "file:")
     let anchorTags = document.getElementsByTagName("a"); // Get all anchor tags
     for (let anchor of anchorTags)
     {
-        if (anchor.href.protocol == "file:" && anchor.href.substring(anchor.href.length - 5, 5) != ".html")
+        if (anchor.href.substring(0, 5) == "file:" && anchor.href.substring(anchor.href.length - 5, 5) != ".html")
         {
             if (anchor.href[anchor.href.length - 1] == "/" || anchor.href[anchor.href.length - 1] == "\\")
             {
