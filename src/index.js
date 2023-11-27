b629c08bdb6212eefd9fcdfd9eecaf77692a6666
qngg <2021xinqing@gmail.com>
2023-11-27 21:08:57 +0800

    cleaner?

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ if (window.location.protocol === "file:")
     let anchorTags = document.getElementsByTagName("a"); // Get all anchor tags
     for (let anchor of anchorTags)
     {
-        if (anchor.href.anchorTags != "http:" || anchor.href.anchorTags != "https:")
+        let anchorHyperlink = anchor.href;
+        if (anchorHyperlink.anchorTags == "file:" && anchorHyperlink.substring(anchorHyperlink.length - 5, 5) != ".html")
         {
-            if (anchor.href[anchor.href.length - 1] == "/" || anchor.href[anchor.href.length - 1] == "\\")
+            if (anchorHyperlink[anchorHyperlink.length - 1] == "/" || anchorHyperlink[anchorHyperlink.length - 1] == "\\")
             {
-                anchor.href += "index.html";
+                anchorHyperlink += "index.html";
             }
             else
             {
-                anchor.href += "/index.html";
+                anchorHyperlink += "/index.html";
             }
         }
     }
